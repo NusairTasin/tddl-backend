e4e0e1fee1dedf09f54f869d92c32a1763127e15
NusairTasin <tasinnusair@gmail.com>
2025-06-14 18:41:54 +0600

    Refactor v2

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,6 @@
 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { useEffect, useState } from "react";
 
 export default function Layout({ children}: { children: React.ReactNode}) {
 
